e18b17c8454494f5415a0630130d6b78150f68eb
Raymond Chen <ahrchen@gmail.com>
2022-04-21 11:46:48 -0700

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Raymond Chen on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
