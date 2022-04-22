457371e68470cbfdc253aac392d624401c4c6e4b
Raymond Chen <ahrchen@gmail.com>
2022-04-22 12:00:54 -0700

    Build primary list of items

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = ""
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink {
+                    Text(resort.name)
+                } label: {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 1)
+                        )
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
+        }
+        
+    }
+    
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            return allNames
+        } else {
+            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
     }
 }
 
